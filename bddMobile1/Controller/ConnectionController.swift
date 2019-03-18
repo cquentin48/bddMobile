c377b7051f047db1544a1ee531e46d4fcf125dfc
Quentin CHAPEL <chapelquen@gmail.com>
2019-03-18 12:06:53 +0100

    [Feat]Init connection storyboard
    Adding connection storyboard to project

---
+++ bddMobile1/Controller/ConnectionController.swift
@@ -0,0 +1,30 @@
+//
+//  ConnectionController.swift
+//  bddMobile1
+//
+//  Created by lpiem on 18/03/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
